f8a3831032234c65230ab327a7922388b484f6e4
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-12-11 15:16:04 +0100

    Add income calculation

--- src/mscn/profit_calculator.py
+++ src/mscn/profit_calculator.py
@@ -18,7 +18,9 @@ class ProfitCalculator:
         tranportation_costs = self._calculate_transportation_costs()
         contract_costs = self._calculate_contract_costs()
         income = self._calculate_income()
-        return income - contract_costs - tranportation_costs
+        profit = round(income - contract_costs - tranportation_costs, 2)
+        print(f"PROFIT $$$: {profit}")
+        return profit
 
     def _calculate_partial_trans_cost(self, paths, num_of_entities_from, num_of_entities_to, transactions):
         total_cost = 0.0
@@ -50,14 +52,28 @@ class ProfitCalculator:
             num_of_entities_to=self._mscn.shops_count,
             transactions=self._mscn.warehouse_shop_transactions
         )
-        total_transportation_cost = supp_to_fact_trans_cost + fact_to_wareh_trans_cost + wareh_to_shop_trans_cost
-        print(f"Total trans cost: {round(total_transportation_cost, 2)}")
-        return round(total_transportation_cost, 2)
+        total_transportation_cost = round(supp_to_fact_trans_cost + fact_to_wareh_trans_cost + wareh_to_shop_trans_cost, 2)
+        print(f"Total trans cost: {total_transportation_cost}")
+        return total_transportation_cost
 
     def _calculate_contract_costs(self) -> float:
         return 0.0
 
     def _calculate_income(self) -> float:
-        return 0.0
+        wareh_shops_paths=self._solution_splitter.get_warehouses_to_shops_partial_solution()
+        shops_amount: List[float] = [0.0 for _ in range(self._mscn.shops_count)]
+
+        for wareh_idx in range(self._mscn.warehouses_count):
+            for shop_idx in range(self._mscn.shops_count):
+                idx = wareh_idx*self._mscn.warehouses_count + shop_idx
+                delivered_amount = wareh_shops_paths[idx]
+                shops_amount[shop_idx] += delivered_amount
+
+        all_shops_income = 0.0
+        for idx, shop_amount in enumerate(shops_amount):
+            all_shops_income += shop_amount*self._mscn.shops[idx].income_per_product
+        all_shops_income = round(all_shops_income, 2)
+        print(f"Total income: {all_shops_income}")
+        return all_shops_income
 
 
